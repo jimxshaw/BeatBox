f2b6138b9a4eca3c6272d4bcd8b2bfeeedbbb838
Jim Shaw <jimxshaw@gmail.com>
2022-01-29 14:45:30 -0600

    Add SoundHolder inner class in MainActivity.

--- app/src/main/java/me/jimmyshaw/beatbox/MainActivity.kt
+++ app/src/main/java/me/jimmyshaw/beatbox/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import me.jimmyshaw.beatbox.databinding.ActivityMainBinding
+import me.jimmyshaw.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Data binding library auto-generates ActivityMainBinding, which holds on
+        // (1) Data binding library auto-generates ActivityMainBinding, which holds on
         // to the view hierarchy in the root property. The binding also holds on
         // to named references tagged with an android:id in the layout file.
         val binding: ActivityMainBinding =
@@ -19,5 +21,11 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
         }
+    }
+
+    // (2) Data binding library auto-generates ListItemSoundBinding from list_item_sound.xml.
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
     }
 }
