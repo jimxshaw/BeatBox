bded3b79e0d079d0135c44bfcaf58614c9a4e828
Jim Shaw <jimxshaw@gmail.com>
2022-01-29 14:32:56 -0600

    Add data binding to MainActivity with ActivityMainBinding object.

--- app/src/main/java/me/jimmyshaw/beatbox/MainActivity.kt
+++ app/src/main/java/me/jimmyshaw/beatbox/MainActivity.kt
@@ -2,10 +2,22 @@ package me.jimmyshaw.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import me.jimmyshaw.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Data binding library auto-generates ActivityMainBinding, which holds on
+        // to the view hierarchy in the root property. The binding also holds on
+        // to named references tagged with an android:id in the layout file.
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
     }
 }
